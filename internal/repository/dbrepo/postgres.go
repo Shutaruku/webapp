9885cb075979a30137f5497882f07f1b2ec1a17c
Jinyuan <kcykey@gmail.com>
2024-02-14 13:46:03 +0800

    Add tests

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -3,6 +3,7 @@ package dbrepo
 import (
 	"context"
 	"errors"
+	"log"
 	"time"
 
 	"github.com/YuanData/webapp/internal/models"
@@ -537,4 +538,33 @@ func (m *postgresDBRepo) GetRestrictionsForBungalowByDate(bungalowID int, start,
 
 	return restrictions, nil
 
+}
+
+func (m *postgresDBRepo) InsertBlockForBungalow(id int, startDate time.Time) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `insert into bungalow_restrictions (start_date, end_date, bungalow_id, restriction_id,
+			created_at, updated_at) values ($1, $2, $3, $4, $5, $6)`
+
+	_, err := m.DB.ExecContext(ctx, query, startDate, startDate.AddDate(0, 0, 1), id, 2, time.Now(), time.Now())
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (m *postgresDBRepo) DeleteBlockByID(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `delete from bungalow_restrictions where id = $1`
+
+	_, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
